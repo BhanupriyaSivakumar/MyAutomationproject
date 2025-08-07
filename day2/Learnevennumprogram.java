e50e8a4fae32f3c57305434d722c8c0c98eedde3
BhanupriyaSivakumar <ss.priyaa07@gmail.com>
2025-08-08 00:42:41 +0530

    Add files via upload
    day2

---
+++ day2/Learnevennumprogram.java
@@ -0,0 +1,20 @@
+package seleniumweek1.day2;
+
+public class Learnevennumprogram {
+	
+	public static void main(String[] args) {
+			
+			for (int i = 1; i <=10; i++) {
+				
+				if (i%2==0) {
+					System.out.println(i);
+					
+				}
+				
+			}
+
+		}
+
+	}
+
+
---
+++ day2/Learoddnumberday2.java
@@ -0,0 +1,17 @@
+package seleniumweek1.day2;
+
+public class Learoddnumberday2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=10; i++) {
+			
+			if (i%2==1) {
+				System.out.println(i);
+				
+			}
+			
+		}
+
+	}
+
